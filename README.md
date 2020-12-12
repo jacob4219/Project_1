908ecef34640901aef76114cb6d9812b1ca99b49
Jacob Solano <jacobsolano1988@gmail.com>
2020-12-12 08:59:23 -0600

    Before Class Commit. My images do not seem to be working in relative, this may have to be switched later.

--- README.md
+++ README.md
@@ -1,8 +1,32 @@
 # This is my Read Me
-## h2
-### h3
-#### h4
 
-[this is a link] (http://google.com/example)
+Name:
+The Man In The Machine
 
-![this is an image] (Image.com/location) 
+Description:
+This is a game that I based loosely off of experiencing the "ilovebees.com" halo marketing campaign when I was young.
+The game begins by opening up an oudated and barely functional "Virtual Desktop". 
+By simply exploring what is interactive, functions are triggered and eventually the game begins.
+
+The Game:
+A man is mysteriously imprisoned in a machine. Help him escape by answering questions to unlock his prison.
+
+Visuals
+The entire thing is meant to be a mysterious experience. Moving through it without direction, happening upon things, is the general idea. A single background image was used and 5 icons to create a desktop experience.
+
+Technologies Used:
+- HTML
+- CSS
+- JavaScript
+
+Devlopment Plan
+I sketched out the flow of the website on several sheets of paper, free hand.
+
+Support
+jacobsolano1988@gmail.com
+
+Built and Designed By:
+Jacob Solano
+
+Project status
+In development. The answering system is broken and I have disabled the time limit while working on it. 
--- js/app.js
+++ js/app.js
@@ -1,114 +1,307 @@
-$(() => {
-    // Cache My Dom Nodes:
-    const $form = $('form');
+// An alert to set the tone and story
+alert(
+  "Due to the passing of our beloved founder Dr. Goodman, Virtual Desktop Pro (trial edition) will no longer be supported or maintained. The entire VDP(te) team thanks you all for the many years of loyalty. In accordance with Dr. Goodman's last will and testament the site will remain online, in its final state, indefinitely.    - VDP(te) Development Team"
+);
 
-    // Input Form
-    $form.on('send', (event) => {
-        event.preventDefault();
-        console.log($('#input-box').val());
-        $(event.currentTarget).trigger('reset');
-    })
+///////////////////////////////////////////////////
+//// GAME ////
+///////////////////////////////////////////////////
+
+// creating constants to be able to attach functions to my buttons
+const startButton = document.getElementById("startButton");
+const nextButton = document.getElementById("nextButton");
+const questionContainerElement = document.getElementById("gameQuestions");
+const questionElement = document.getElementById("question");
+const answerButtonsElement = document.getElementById("answerButtons");
+
+//shuffle through my questions array
+let shuffleQuestions, currentQuestionIndex;
+
+// add event listeners to my game play buttons and allow the next button to go through my questions array.
+startButton.addEventListener("click", startGame)
+nextButton.addEventListener("click", () => {
+  currentQuestionIndex++
+  nextQuestion();
 });
 
-// Input button
-
-const list = [];
-
-$('form').on('send', (event) => {
-    console.log($('#input-box').val());
-
-    list.push($('#input-box').val());
-
-    event.preventDefault();
-    $(event.currentTarget).trigger('reset');
-
-    render();
-});
-
-// Render The To Do List
-
-const render = () => {
-    console.log('list: ', list);
-    $('#anonTalk').empty();
-    list.forEach((item) => {
-        $('#anonTalk').append('<li>' + item + '</li>');
-        console.log(item + " Was not sent.");
-    });
-
-// Render To The Completed List
-    $('li').on('click', (event) => {
-        $(event.currentTarget).remove('#anonTalk');
-        $('#drTalk').prepend(event.currentTarget);
-
-        $(event.currentTarget).css('text-decoration', 'line-through');
-        console.log("A Task Has Been Completed!");
-        alert("A Task Has Been Completed!");
-
-    })
-};
-
-// Added A Task Alert
-
-const addedTask = () => {
-    alert("The recipient cannot accept incoming messages.");
-    console.log("Unable To Send...");
+// clicking the start button makes it and the intro window disappear. It also makes the question container appear and begin on a random question.
+function startGame() {
+  console.log("The Man In The Machine Has Begun")
+  startButton.classList.add("hide");
+  shuffleQuestions = questions.sort(() => Math.random() - 0.5)
+  currentQuestionIndex = 0
+  questionContainerElement.classList.remove("hide")
+  nextQuestion();
 }
 
-document.getElementById("addTaskBtn").addEventListener("click", addedTask);
-
-
-//// Popup Window
-
-const el = document.querySelector(".chat");
-
-el.addEventListener('mousedown', mousedown);
-
-function mousedown(e) {
-    window.addEventListener('mousemove', mousemove);
-    window.addEventListener('mouseup', mouseup);
-
-    let prevX = e.clientX;
-    let prevY = e.clientY;
-    
-    function mousemove(e) {
-        let newX = prevX - e.clientX;
-        let newY = prevY - e.clientY;
-
-        const rect = el.getBoundingClientRect();
-
-        el.style.left = rect.left - newX + "px";
-        el.style.top = rect.top - newY + "px";
-
-        prevX = e.clientX;
-        prevY = e.clientY;
-
-    }
-
-    function mouseup(){
-        window.removeEventListener('mousemove', mousemove);
-        window.removeEventListener('mouseup', mouseup);
-
-    }
-
-    const resizers = document.querySelectorAll(".resizer");
-    let currentResizer;
-
-    for(let resizer of resizers) {
-        resizer.addEventListener('mousedown', mousedown);
-
-        function mousedown(e) {
-            currentResizer = e.target;
-
-            let prevX = e.clientX;
-            let prevY = e.clientY;
-
-            window.addEventListener('mousemove', mousemove);
-            window.addEventListener('mouseup', mouseup);
-
-            function mousemove(e) {
-
-            }
-
-        }
-    }
+// allows for a reset state.
+function nextQuestion() {
+  resetState();
+  showQuestion(shuffleQuestions[currentQuestionIndex])
 }
+
+
+function showQuestion(question) {
+  questionElement.innerText = question.question;
+  question.answers.forEach((answer) => {
+    const button = document.createElement("button")
+    button.innerText = answer.text;
+    button.classList.add("answerBtn")
+    if (answer.correct) {
+      button.dataset.correct = answer.correct
+    }
+    button.addEventListener("click", selectAnswer)
+    answerButtonsElement.appendChild(button)
+  });
+}
+
+function resetState() {
+  clearStatusClass(document.body)
+  nextButton.classList.add("hide")
+  while (answerButtonsElement.firstChild) {
+    answerButtonsElement.removeChild(answerButtonsElement.firstChild)
+  }
+}
+
+function selectAnswer(e) {
+  const selectedButton = e.target
+  const correct = selectedButton.dataset.correct
+  setStatusClass(document.body, correct)
+  Array.from(answerButtonsElement.children).forEach((button) => {
+    setStatusClass(button, button.dataset.correct)
+  });
+  if (shuffleQuestions.length > currentQuestionIndex + 1) {
+    nextButton.classList.remove("hide");
+  } else {
+    startButton.innerText = "Restart";
+    startButton.classList.remove("hide");
+  }
+}
+
+function setStatusClass(element, correct) {
+  clearStatusClass(element);
+  if (correct) {
+    element.classList.add("correct");
+  } else {
+    element.classList.add("wrong");
+  }
+}
+
+function clearStatusClass(element) {
+  element.classList.remove("correct");
+  element.classList.remove("wrong");
+}
+
+//////////////////////////////
+//// Questions ////
+//////////////////////////////
+
+const questions = [
+  {
+    question: "Is the world changing?",
+    answers: [
+      { text: "Yes", correct: true },
+      { text: "No", correct: false },
+    ],
+  },
+  {
+    question: "15 + 15 = ",
+    answers: [
+      { text: "30", correct: true },
+      { text: "90", correct: false },
+      { text: "20", correct: false },
+      { text: "250", correct: false },
+    ],
+  },
+  {
+    question: "Are you sure you want to proceed?",
+    answers: [
+      { text: "Yes", correct: true },
+    ],
+  },
+  {
+    question: "What color is the sky?",
+    answers: [
+      { text: "green", correct: false },
+      { text: "beige", correct: false },
+      { text: "red", correct: false },
+      { text: "blue", correct: true },      
+    ],
+  },
+  {
+    question: "What's cooler than being cool?",
+    answers: [
+      { text: "The Other Side Of The Pillow", correct: false },
+      { text: "Ice Cold", correct: true },
+      { text: "Big Cats", correct: false },
+      { text: "Big Dogs", correct: false },      
+    ],
+  },
+];
+
+
+
+///////////////////////////////////////
+//// Icons ////
+///////////////////////////////////////
+// Chat Icon //
+const chatIcon = document.getElementById("vdpChatIcon");
+
+vdpChatIcon.addEventListener("click", clickChat);
+
+function clickChat() {
+  alert(
+    "Chat is not available in trial mode. Unlock premium now to use this feature!"
+  );
+}
+
+// Browser Icon //
+const browserIcon = document.getElementById("vdpBrowserIcon");
+
+vdpBrowserIcon.addEventListener("click", clickBrowser);
+
+function clickBrowser () {
+  alert(
+    "Browser is not available in trial mode. Unlock premium now to use this feature!"
+  );
+}
+// Cloud Storage Icon //
+const storageIcon = document.getElementById("vdpStorageIcon");
+
+vdpStorageIcon.addEventListener("click", clickStorage);
+
+function clickStorage () {
+  alert(
+    "Cloud Storage is not available in trial mode. Unlock premium now to use this feature!"
+  );
+}
+// Unlock Premium Icon //
+const purchase = document.getElementById("buyPremium");
+const openMessage = document.getElementById("chat");
+
+vdpPremiumIcon.addEventListener("click", buyPremium);
+closePremiumBtn.addEventListener("click", closePremiumWindow);
+closePremiumBtn.addEventListener("click", openChatWindow);
+
+function buyPremium() {
+  purchase.classList.remove("hide");
+}
+
+function closePremiumWindow() {
+  purchase.classList.add("hide");
+
+}
+
+
+
+// Open Anonymous Chat and Receive Game Icon //
+function openChatWindow () {
+    openMessage.classList.remove("hide");
+}
+
+const sendMessage = document.getElementById("sendButton");
+
+sendMessage.addEventListener("click", clickSend)
+
+const openPrison = document.getElementById("prison");
+
+sendMessage.addEventListener("click", openPrisonIcon)
+
+function clickSend() {
+  alert("VDP(te) no longer supports chat functionality.");
+  event.preventDefault();
+}
+
+function openPrisonIcon() {
+  openPrison.classList.remove("hide")
+}
+
+// Open Game //
+
+const openGame = document.getElementById("game")
+
+prisonIcon.addEventListener("click", openGamePlay)
+
+function openGamePlay () {
+  openGame.classList.remove("hide")
+}
+
+const makeMessage = document.getElementById("startMessage")
+
+prisonIcon.addEventListener("click", createMessage)
+
+function createMessage () {
+  makeMessage.classList.remove("hide")
+}
+
+startButton.addEventListener("click", removeMessage)
+
+function removeMessage () {
+  makeMessage.classList.add("hide")
+}
+
+const lockVisual = document.getElementById("visualLock")
+
+startButton.addEventListener("click", onVisual)
+
+function onVisual () {
+  lockVisual.classList.remove("hide")
+}
+
+startButton.addEventListener("click", trialExpired)
+function trialExpired () {
+  alert("You have 1 minute remaining on your free trial. Unlock premium to enjoy unlimited VDP!!!");
+}
+
+// Timer
+startButton.addEventListener("click", timesUP)
+
+function timesUP() {
+  alert("Your free trial has expired. Thanks for enjoying Virtual Desktop Pro!")
+}
+
+// Clock
+var myVar = setInterval(myTimer, 1000);
+
+function myTimer() {
+  var d = new Date();
+  document.getElementById("clock").innerHTML = d.toLocaleTimeString();
+}
+
+
+
+
+
+/////////////////////////////////////////////////
+// Drag Windows ////
+/////////////////////////////////////////////////
+
+// const dragAround = document.querySelector("#chat")
+
+// dragAround.addEventListener("mousedown", mousedown)
+
+// function mousedown(e) {
+//   window.addEventListener("mousemove", mousemove);
+//   window.addEventListener("mouseup", mouseup);
+
+//   let prevX = e.clientX;
+//   let prevY = e.clientY;
+
+//   function mousemove(e) {
+//     let newX = prevX - e.clientX;
+//     let newY = prevY - e.clientY;
+
+//     const rect = dragAround.getBoundingClientRect();
+
+//     dragAround.style.left = rect.left - newX + "px";
+//     dragAround.style.top = rect.top - newY + "px";
+
+//     // prevX = e.clientX;
+//     // prevY = e.clientY;
+//   }
+
+//   function mouseup() {
+//     window.removeEventListener("mouseup", mouseup);
+//     window.removeEventListener("mousemove", mousemove);
+//   }
+// }
